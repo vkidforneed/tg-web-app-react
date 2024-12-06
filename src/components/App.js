6ddf9779dca12a8e5b02a0e2d6bc96e5848597f2
vkidforneed <marrkkred@gmail.com>
2024-12-06 23:11:53 +0700

    init

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,25 @@
 import './App.css';
 import {useEffect} from "react";
-const  tg = window.Telegram.WebApp;
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
+    const {onToggleButton, tg} = useTelegram();
 
     useEffect(() => {
         tg.ready();
-    }, []);
-
-    const onClose = () => {
-        tg.close()
-    }
+    }, [])
 
     return (
         <div className="App">
-            work ebat
-            <button onClick={onClose}>Закрыть</button>
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
         </div>
     );
 }
